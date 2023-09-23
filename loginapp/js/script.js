d086ef3ffd92bc57fdcb390e5006ace68ad3e6dd
maryyygb <138882821+maryyygb@users.noreply.github.com>
2023-09-22 20:04:48 -0600

    01

---
+++ loginapp/js/script.js
@@ -0,0 +1,21 @@
+const APIKEY = "04c35731a5ee918f014970082a0088b1";
+const APIURL = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=d183e60f91f44f9fd932a595c8654fc6&page=1";
+const IMGPATH = "https://image,tmdb.org/t/p/w1280";
+
+// async function getMovies() {
+//     const resp = await fetch(APIURL);
+//     const respData = await resp.json();
+
+//     console.log(respData);
+
+//     respData.results.forEach((movie) => {
+//         const img = document.createElement("img");
+//         img.src = IMGPATH + movie.poster_path;
+
+//         document.body.appendChild(img);
+//     });
+
+//     return respData;
+// }
+
+// getMovies();
