50394e2e61ae93d762e46d4742273a633666c0c9
maryyygb <138882821+maryyygb@users.noreply.github.com>
2023-09-23 08:02:30 -0600

    05

--- js/script.js
+++ js/script.js
@@ -1,23 +1,44 @@
 const APIKEY = "04c35731a5ee918f014970082a0088b1";
 const APIURL = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=d183e60f91f44f9fd932a595c8654fc6&page=1";
-const IMGPATH = "https://image,tmdb.org/t/p/w1280";
+const IMGPATH = "https://image.tmdb.org/t/p/w1280";
 
-document.getElementById("moneyVideo").loop = true;
+// document.getElementById("moneyVideo").loop = true;
 
-// async function getMovies() {
-//     const resp = await fetch(APIURL);
-//     const respData = await resp.json();
+const main = document.querySelector('main');
 
-//     console.log(respData);
+async function getMovies() {
+    const resp = await fetch(APIURL);
+    const respData = await resp.json();
 
-//     respData.results.forEach((movie) => {
-//         const img = document.createElement("img");
-//         img.src = IMGPATH + movie.poster_path;
+    console.log(respData);
 
-//         document.body.appendChild(img);
-//     });
+    respData.results.forEach((movie) => {
+        const { poster_path, title, vote_average } = movie
 
-//     return respData;
-// }
+        const movieEl = document.createElement("div");
+        movieEl.classList.add("movie");
 
-// getMovies();
+        movieEl.innerHTML = `
+
+    <img 
+    src="${IMGPATH + poster_path}"
+    alt="${title}"
+    />
+
+        <div class="movie-info">
+        <h3>${title}</h3>
+        <span>${vote_average}</span>
+        
+    </div>
+        `;
+
+        main.appendChild(movieEl);
+
+
+    });
+
+
+    return respData;
+}
+
+getMovies();
